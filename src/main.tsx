7b30b9a1688c8aabf69612f611a3654d102e668f
mac.yangfan <353848181@qq.com>
2026-05-26 22:23:27 +0800

    添加 vConsole 用于 APK 中调试日志

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-/**
- * 应用入口
- * 将 React 应用挂载到 #root 元素上，启用 StrictMode 进行开发检查
- */
+if (!import.meta.env.DEV) {
+  import('vconsole').then(({ default: VConsole }) => {
+    new VConsole()
+  })
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
